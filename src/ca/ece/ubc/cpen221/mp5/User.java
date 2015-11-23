8c5a54d332251e9a422d59778d9f8f4581e6f35b
Ryan Cotsakis <ryancotsakis@gmail.com>
2015-11-23 12:20:12 -0800

    started user.java

--- src/ca/ece/ubc/cpen221/mp5/User.java
+++ src/ca/ece/ubc/cpen221/mp5/User.java
@@ -1,7 +1,14 @@
 package ca.ece.ubc.cpen221.mp5;
 
+import java.util.*;
+
 // TODO: Use this class to represent a Yelp user.
 
 public class User {
-
+	private Map<String, Integer> votes;
+	private int reveiw_count;
+	private String user_id;
+	private int 
+	
+	
 }

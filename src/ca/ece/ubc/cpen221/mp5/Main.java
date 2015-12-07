ec74b6764ca7aa9d504ec79db442e6146c2231f4
Ryan Cotsakis <ryancotsakis@gmail.com>
2015-12-07 12:32:53 -0800

    made client

--- src/ca/ece/ubc/cpen221/mp5/Main.java
+++ src/ca/ece/ubc/cpen221/mp5/Main.java
@@ -4,7 +4,7 @@ public class Main{
 
 	public static void main (String[] args){
 
-		//RestaurantDBServer(int port, String restaurants, String reviews, String users)
+		//RestaurantDBServer(4444 data/restaurants.json data/reviews.json data/users.json)
 		new RestaurantDBServer(Integer.parseInt(args[0]), args[1], args[2], args[3]);
 	}
 }
---
+++ src/ca/ece/ubc/cpen221/mp5/RestaurantDBClient.java
@@ -0,0 +1,41 @@
+package ca.ece.ubc.cpen221.mp5;
+
+import java.io.*;
+import java.net.*;
+
+public class RestaurantDBClient{
+
+	public RestaurantDBClient(String hostname, int port) {
+		try{
+			Socket socket = new Socket(hostname, port);
+			PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+	
+			String fromServer;
+			String fromUser;
+	
+			do{
+				fromServer = input.readLine();
+				if (fromServer != null) {
+					System.out.println("Server: " + fromServer);
+					fromServer = null;
+				}
+				fromUser = stdIn.readLine();
+				if (fromUser != null) {
+					System.out.println("Client: " + fromUser);
+					output.println(fromUser);
+				}
+			}while(!fromUser.equals("exit"));
+			socket.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		new RestaurantDBClient("localhost", 4444);
+	}
+}

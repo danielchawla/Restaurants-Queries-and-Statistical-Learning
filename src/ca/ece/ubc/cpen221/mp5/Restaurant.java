2e1bc12906256ccd912dc1f16ece70f6cf0beefc
Daniel Chwala <danielchawla93@gmail.com>
2015-12-07 15:46:48 -0800

    updated specs for restaurants

--- src/ca/ece/ubc/cpen221/mp5/Restaurant.java
+++ src/ca/ece/ubc/cpen221/mp5/Restaurant.java
@@ -5,9 +5,6 @@ import java.util.*;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 
-// TODO: Use this class to represent a restaurant.
-// State the rep invariant and abs
-
 public class Restaurant {
     
     final private JSONObject restaurantJSON;
@@ -39,9 +36,9 @@ public class Restaurant {
     final private String type;
     final private Set<String> categories = new HashSet<String>();
     
-    final private double stars; // rep invariant: 0 <= stars <= 5.0
-    private long reviewCount; // rep invariant: review >= 0
-    final private long price; // rep invariant: 1 <= price <= 4
+    final private double stars; 
+    private long reviewCount; 
+    final private long price; 
     final private String photo;
     final private Set<String> schools = new HashSet<String>();
 
@@ -51,6 +48,17 @@ public class Restaurant {
      * @param object - the restaurant details in JSON format
      */
     public Restaurant(JSONObject object) {
+        // 
+        // Abstraction function: represents physical restaurant that has chosen to be added to yelp database.     
+        //
+        // rep invariant:
+        //      - no fields can be null
+        //      - 1.0 <= price <= 4.0
+        //      - review >= 0.0
+        //      - 1.0 <= stars <= 5.0
+        //      - businessID is unique
+        //      - schools, city, address, and state are real and accurate
+        //      - coordinates are real and represent actual location on earth
         
         this.restaurantJSON = (JSONObject) object.clone();
         this.name = (String) this.restaurantJSON.get(NAME);
@@ -83,79 +91,142 @@ public class Restaurant {
         }
     }
     
+    /**
+     * Clones restaurant.
+     */
     public Restaurant clone() {
         return new Restaurant(this.restaurantJSON);
     }
-
+    
+    /**
+     * Retrieves name of restaurant
+     * @return name
+     */
     public String getName() {
         return this.name;
     }
-
+    
+    /**
+     * Returns business ID of restaurant.
+     * @return business ID
+     */
     public String getBusinessID() {
         return this.businessID;
     }
     
+    /**
+     * Returns type of object. All these should be of the restaurant type.
+     * @return type
+     */
     public String getType() {
         return this.type;
     }
-
+    
+    /**
+     * Retrieves city containing restaurant.
+     * @return city
+     */
     public String getCity() {
         return this.city;
     }
 
+    /**
+     * Retrieves address of restaurant
+     * @return address
+     */
     public String getAddress() {
         return this.address;
     }
 
+    /**
+     * Retrieves state restaurant is in.
+     * @return state
+     */
     public String getState() {
         return this.state;
     }
     
+    /**
+     * Retrieves location of restaurant
+     * @return coordinates of restaurant.
+     */
     public Location getLocation() {
         return coordinates.clone();
     }
     
+    /**
+     * Retrieves restaurant details in JSON format
+     * @return string of restaurant details
+     */
     public String getJSON() {
         return this.restaurantJSON.toJSONString();
     }
 
+    /**
+     * Retrieves neighborhood of restaurant.
+     * @return neighborhood
+     */
     public Set<String> getNeighbors() {
         Set<String> neighbourhoodDuplicate = new HashSet<String>();
         neighbourhoodDuplicate.addAll(neighbourhood);
         return Collections.unmodifiableSet(neighbourhoodDuplicate);
     }
 
+    /**
+     * Retrieves categories restaurant satisfies.
+     * @return categories
+     */
     public Set<String> getCategories() {
         Set<String> categoriesClone = new HashSet<String>();
         categoriesClone.addAll(categories);
         return Collections.unmodifiableSet(categoriesClone);
     }
 
+    /**
+     * Returns avg number of stars users have rated restaurant to have.
+     * @return avg number of stars.
+     */
     public double getStars() {
         return this.stars;
     }
-
+    
+    /**
+     * Retrieves number of reviews restaurant has.
+     * @return num of reviews
+     */
     public long getReviewCount() {
         return this.reviewCount;
     }
 
+    /**
+     * Retrieves price range of restaurant.
+     * @return price range
+     */
     public long getPrice() {
         return this.price;
     }
 
+    /**
+     * Retrieves Restaurant photo
+     * @return photo
+     */
     public String getPhoto() {
-        return photo;
+        return this.photo;
+    }
+    
+    /**
+     * Retrieves set of all schools
+     * @return all schools
+     */
+    public Set<String> getSchools() {
+        Set<String> schoolsDuplicate = new HashSet<String>();
+        schoolsDuplicate.addAll(schools);
+        return Collections.unmodifiableSet(schoolsDuplicate);
     }
     
     @Override
     public String toString() {
         return this.name;
-    }
-
-    public Set<String> getSchools() {
-        Set<String> schoolsDuplicate = new HashSet<String>();
-        schoolsDuplicate.addAll(schools);
-        return Collections.unmodifiableSet(schoolsDuplicate);
     }
 
     @Override
